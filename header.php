f4f060d718b875201910069b394ae950d0256597
JoshThomson7 <josh@fl1.digital>
2023-11-23 13:41:35 +0000

    Initial changes

--- header.php
+++ header.php
@@ -25,35 +25,8 @@
 
                 <div class="header__main">
                     <div class="max__width">
-                        <div class="header__main--left">
-                            <div class="logo">
-                                <a href="<?php echo esc_url(home_url()); ?>" title="<?php bloginfo('name'); ?>">
-                                    <img src="<?php echo esc_url(get_stylesheet_directory_uri().'/img/logo.png'); ?>" alt="<?php bloginfo('name'); ?>"/>
-                                </a>
-                            </div>
-
+                        <div class="header__main--left">                            
                             <?php include get_stylesheet_directory().'/modules/mega-menu.php'; ?>
-                        </div>
-
-                        <div class="header__main--right">
-                            <ul>
-                                <li>
-                                    <a href="<?php echo wc_get_cart_url(); ?>">
-                                        <i class="fal fa-basket-shopping-simple"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="spotlight-open">
-                                        <i class="fal fa-search"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="/products" class="button primary small icon-right">
-                                        <span>Buy Now</span>
-                                        <i class="fa fa-chevron-right"></i>
-                                    </a>
-                                </li>        
-                            </ul>
 
                             <a href="#nav_mobile" class="burger__menu">
                                 <i class="fal fa-bars"></i>
